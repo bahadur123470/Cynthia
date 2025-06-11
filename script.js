1cbc058f8078905c231028e7c5810200334695dc
Bahadur Ali <bahadurali123470@gmail.com>
2025-06-11 21:05:19 +0500

    Refactor cursor follower implementation and enhance animations for improved user experience

--- script.js
+++ script.js
@@ -1,14 +1,66 @@
-// import LocomotiveScroll from 'locomotive-scroll';
-
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
 
-function circleMouseFollower (){
-    window.addEventListener("mousemove", function(det){
-        document.querySelector("#mousefollower").style.transform = `translate(${det.clientX}px, ${det.clientY}px)`;
+let initial ;
+function skewCursor(){
+    let xScale = 1;
+    let yScale = 1;
+
+    let prevX = 0;
+    let prevY = 0;
+    window.addEventListener("mousemove", (dets)=> {
+        clearTimeout(initial)
+        xScale = gsap.utils.clamp(.8, 1.2, dets.clientX - prevX);
+        yScale = gsap.utils.clamp(.8, 1.2, dets.clientY - prevY);
+
+        prevX = dets.clientX;
+        prevY = dets.clientY
+
+        dotCursorFollower(xScale, yScale)
+
+        initial = setTimeout(function () {
+        document.querySelector("#cursorfollower").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+        }, 100)
+    });
+}
+
+function dotCursorFollower(xScale, yScale) {
+    window.addEventListener("mousemove", function(dets){
+        document.querySelector("#cursorfollower").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xScale}, ${yScale})`;
     })
 }
 
-circleMouseFollower();
+function landingPage () {
+    let anime = gsap.timeline();
+
+    anime.from("#nav", {
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+    anime.to(".textup", {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 1,
+        stagger: .2
+    })
+    anime.from(".textdown, #sub2", {
+        y: -10,
+        opacity: 0,
+        duration: 1,
+        ease: Expo.easeInOut
+    })
+    // anime.from("#sub2", {
+    //     y: -10,
+    //     opacity: 0,
+    //     duration: 1,
+    //     ease: Expo.easeInOut
+    // })
+}
+
+skewCursor();
+dotCursorFollower();
+landingPage();
